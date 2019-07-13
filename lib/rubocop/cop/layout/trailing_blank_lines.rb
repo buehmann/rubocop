17917fc5ae685e4645f0b608808dfac51f38d6b8
Andreas Bühmann <dev@uuml.de>
2019-07-13 15:07:15 +0200

    Fix __END__ detection

--- lib/rubocop/cop/layout/trailing_blank_lines.rb
+++ lib/rubocop/cop/layout/trailing_blank_lines.rb
@@ -87,7 +87,7 @@ module RuboCop
           return true if buffer.source.strip.start_with?('__END__')
           return false if processed_source.tokens.empty?
 
-          extra = buffer.source[processed_source.tokens.last.end_pos..-1]
+          extra = buffer.source[processed_source.sorted_tokens.last.end_pos..-1]
           extra&.strip&.start_with?('__END__')
         end
 
